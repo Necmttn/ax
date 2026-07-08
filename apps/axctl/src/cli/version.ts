8d726d479de0bf4e25a820d7d538fad7ec3faa9c
Neco <necmettin.karakaya@gmail.com>
2026-07-08 21:21:40 +0800

    chore(main): release 0.37.1 (#674)

--- apps/axctl/src/cli/version.ts
+++ apps/axctl/src/cli/version.ts
@@ -1,6 +1,6 @@
 import { spawnSync } from "node:child_process";
 
-export const AX_VERSION = "0.37.0"; // x-release-please-version
+export const AX_VERSION = "0.37.1"; // x-release-please-version
 export const DEFAULT_REPO = "Necmttn/ax";
 
 export interface LatestRelease {
